03e3a54409d564d1da6afc85384065ba2291d0f0
ErkaW <erkaw@outlook.co.id>
2024-05-09 04:06:01 +0000

    a

--- modul7/alloc-first-fit.c
+++ modul7/alloc-first-fit.c
@@ -4,10 +4,10 @@
 void main()
 {
  //set setiap variabel yang dibutuhkan
- int frag[max],ukuranBlok[max],ukuranProses[max],i,j,jmlBlok,jmlProses,temp,lowest=10000;
+ int frag[max],ukuranBlok[max],ukuranProses[max],i,j,jmlBlok,jmlProses,temp;
  static int alokasiBlok[max],flags[max];
  //input dari pengguna
- printf("\nTeknik Alokasi Memori ‐ Best Fit");
+ printf("\nTeknik Alokasi Memori ‐ First Fit");
  printf("\nMasukkan jumlah blok memori:");
  scanf("%d",&jmlBlok);
  printf("Masukkan jumlah proses:");
@@ -24,33 +24,29 @@ void main()
   printf("Proses %d:",i);
   scanf("%d",&ukuranProses[i]);
  }
+ //mengambil setiap proses satu persatu
  for(i=1;i<=jmlProses;i++)
  {
   for(j=1;j<=jmlBlok;j++)
   {
-   //melakukan proses alokasi dengan mengurangi ukuran blok 
-   //dengan ukuran proses
+   //melakukan proses alokasi dengan mengurangi ukuran blok dengan ukuran proses
    if(alokasiBlok[j]!=1)
    {
     temp=ukuranBlok[j]-ukuranProses[i];
     if(temp>=0)
-    if(lowest>temp)
     {
      flags[i]=j;
-     lowest=temp;
+     break;
     }
    }
   }
-  //alokasi best‐fit berdasarkan nilai fragment terkecil
-  frag[i]=lowest;
+  //alokasi first‐fit berdasarkan informasi alokasi blok memori
+  frag[i]=temp;
   alokasiBlok[flags[i]]=1;
-  lowest=10000;
  }
  //tampilan output
  printf("\nNo. Proses:\tUkuran Proses :\tAlokasi Blok:\tUkuran Blok:\tFragment");
  for(i=1;i<=jmlProses;i++)
- printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,ukuranProses[i],flags[i],
- ukuranBlok[flags[i]],frag[i]);
+ printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,ukuranProses[i],flags[i],ukuranBlok[flags[i]],frag[i]);
 }
-
 
